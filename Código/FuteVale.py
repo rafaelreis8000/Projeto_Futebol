3b35225b6a4758b7617cec184d015abf8559d4f2
rafaelreis8000 <priv.rafa.reis@gmail.com>
2024-08-26 16:13:26 -0300

    .

---
+++ Código/FuteVale.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+def main(page: ft.Page):                    #define uma página/ tela principal
+    page.title = 'FuteVale'
+
+    page.window_width = 500
+    page.bgcolor = '#C1BCAC'                #configura o tamanho e as cores da tela
+
+    txt_titulo = ft.Text('Nome do Time: ')
+    NomeTime = ft.TextField(label='Digite o nome do time: ')
+    
+    btn_incluir = ft.ElevatedButton('Cadastrar')
+
+
+    page.add(                               #adiciona os elementos do sistema na tela
+            txt_titulo,
+            NomeTime,
+            btn_incluir
+        )
+
+ft.app(target=main)

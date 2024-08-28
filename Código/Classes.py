8d36669b84d792507a509c4eb60a4ce033807843
rafaelreis8000 <priv.rafa.reis@gmail.com>
2024-08-28 11:50:33 -0300

    .

--- Código/Classes.py
+++ Código/Classes.py
@@ -1,20 +1,22 @@
 class Pessoa:
-    def __init__(self, nome, sobrenome, idade, nascimento, sexo, cargo):
+    def __init__(self, nome, nascimento, sexo):
         self.nome = nome
-        self.sobrenome = sobrenome
-        self.idade = idade
         self.nascimento = nascimento
         self.sexo = sexo
-        self.cargo = cargo
+
+class Clube:
+    def __init__(self, nome):
+        self.nome = nome
+        self.elenco = Elenco
+
+class Elenco:
+    def __init__(self, nome):
+        self.nome = nome
+        self.jogadores = Jogador
 
 class Jogador(Pessoa):
-    def __init__(self, nome, sobrenome, idade, nascimento, sexo, altura, peso, posicao, lateralidade):
-        super().__init__(nome, sobrenome, idade, nascimento, sexo)
+    def __init__(self, nome, nascimento, sexo, altura, peso, lateralidade):
+        super().__init__(nome, nascimento, sexo)
         self.altura = altura
         self.peso = peso
-        self.posicao = posicao
         self.lateralidade = lateralidade
-
-class Cargo(Pessoa):
-    def __init__(self, nome, sobrenome, idade, nascimento, sexo, cargo):
-        super().__init__(nome, sobrenome, idade, nascimento, sexo, cargo)

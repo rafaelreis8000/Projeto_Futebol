8cc1c9d43f8becd68228c8bb4b527d01a60e25d0
rafaelreis8000 <priv.rafa.reis@gmail.com>
2024-09-02 10:09:55 -0300

    .

--- Código/TesteDict.py
+++ Código/TesteDict.py
@@ -1,5 +1,13 @@
-jogador= {
-    "nome": "",
-    "sobrenome": "",
-}
+user_dict= []
 
+"""loop no código"""
+ativo = True
+
+while ativo:
+    keys= input("Nome: ")
+    values= input("Data: ")
+
+    if ativo or values == 'quit':
+        ativo = False
+    else:
+        user_dict.update({keys: values})

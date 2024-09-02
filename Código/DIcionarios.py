03ea0c7e6974ca0ee36ac3d93c55458fa2e4f529
rafaelreis8000 <priv.rafa.reis@gmail.com>
2024-09-02 10:35:31 -0300

    .

---
+++ Código/DIcionarios.py
@@ -0,0 +1,22 @@
+jogador = {
+    "nome": "Benjamin Arrola",
+"posicao": "Lateral Esquerdo"
+}
+
+comissao = {
+    "nome": "Mihan Casprega",
+    "cargo": "Técnico"
+
+}
+
+elenco = {
+    "nome": "Titular",
+    "jogadores": jogador,
+    "comissão": comissao
+}
+
+clube = {
+    "elenco": elenco
+}
+
+print (clube)
---
+++ Código/PlantioDict.py
@@ -0,0 +1,15 @@
+cultura= {
+
+    "nome": "ALFACE",
+    "Data": "10/10/24",
+    "Entrega": "24/12/24"
+
+}
+
+fazenda= {
+
+    "nome": "City Green",
+    "plantio": cultura
+
+}
+print (fazenda)
